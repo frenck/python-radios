25fe2cdd0cb38c252d0b6b92b84e3deca6c61f6d
Franck Nijhof <git@frenck.dev>
2022-02-21 00:58:15 +0100

    Initial version of library

---
+++ examples/example.py
@@ -0,0 +1,43 @@
+# pylint: disable=W0621
+"""Asynchronous Python client for the Radio Browser API."""
+
+import asyncio
+
+from radios import FilterBy, Order, RadioBrowser
+
+
+async def main() -> None:
+    """Show example on how to query the Radio Browser API."""
+    async with RadioBrowser(user_agent="MyAwesomeApp/1.0.0") as radios:
+        # Print top 10 stations
+        stations = await radios.stations(
+            limit=10, order=Order.CLICK_COUNT, reverse=True
+        )
+        for station in stations:
+            print(f"{station.name} ({station.click_count})")
+
+        # Get a specific station
+        print(await radios.station(uuid="9608b51d-0601-11e8-ae97-52543be04c81"))
+
+        # Print top 10 stations in a country
+        stations = await radios.stations(
+            limit=10,
+            order=Order.CLICK_COUNT,
+            reverse=True,
+            filter_by=FilterBy.COUNTRY_CODE_EXACT,
+            filter_term="NL",
+        )
+        for station in stations:
+            print(f"{station.name} ({station.click_count})")
+
+        # Register a station "click"
+        await radios.station_click(uuid="9608b51d-0601-11e8-ae97-52543be04c81")
+
+        # Tags, countries and codes.
+        print(await radios.tags(limit=10, order=Order.STATION_COUNT, reverse=True))
+        print(await radios.countries(limit=10, order=Order.NAME))
+        print(await radios.languages(limit=10, order=Order.NAME))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ tests/test_radios.py
@@ -0,0 +1,9 @@
+"""Asynchronous Python client for the Radio Browser API."""
+# pylint: disable=protected-access
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_dummy() -> None:
+    """Test dummy."""
+    assert True
